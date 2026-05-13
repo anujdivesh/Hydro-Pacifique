81b90a28790ec3e30118cbedea2b6cef48a8961b
Anuj Divesh <divesha@spc.int>
2026-05-13 13:02:55 +1200

    mysql password

--- docker/mysql/init/00-import-demo.sh
+++ docker/mysql/init/00-import-demo.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 set -eu
 
-mysql -uroot -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE" < /seed/20250901_HP_DemoSPC_Data.sql
+MYSQL_ARGS="-uroot"
+
+if [ -n "${MYSQL_ROOT_PASSWORD:-}" ]; then
+	MYSQL_ARGS="$MYSQL_ARGS -p$MYSQL_ROOT_PASSWORD"
+fi
+
+mysql $MYSQL_ARGS "$MYSQL_DATABASE" < /seed/20250901_HP_DemoSPC_Data.sql
